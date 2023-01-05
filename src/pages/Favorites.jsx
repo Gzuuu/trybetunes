c0c2e49f000c848e9fc7703b88e479e084f10eb8
jesue <jesue_gege@hotmail.com>
2023-01-05 17:04:13 -0300

    12 concluida

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class Favorites extends Component {
+  state = {
+    loading: false,
+    favoriteSong: [],
+  };
+
+  componentDidMount() {
+    this.getSavedMusic();
+  }
+
+  getSavedMusic = async () => {
+    const musics = await getFavoriteSongs();
+    this.setState(() => ({
+      loading: true,
+      favoriteSong: musics,
+    }));
+    this.setState(() => ({
+      loading: false,
+    }));
+  };
+
+  deleteSong = async (song) => {
+    this.setState(() => ({
+      loading: true,
+    }));
+    await removeSong(song);
+    const musics = await getFavoriteSongs();
+    this.setState(() => ({
+      loading: false,
+      favoriteSong: musics,
+    }));
+  };
+
   render() {
+    const { favoriteSong, loading } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
+        {loading && <Loading />}
+        {favoriteSong.map((music, index) => (
+          <MusicCard
+            property={ music }
+            key={ index }
+            isFavorite={ favoriteSong.some((favorite) => (
+              favorite.trackName === music.trackName
+            )) }
+            save={ () => this.deleteSong(music) }
+          />
+        ))}
       </div>
     );
   }
