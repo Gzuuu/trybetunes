998646b3c4715863b8a7161e8463ee771bb44f7e
jesue <jesue_gege@hotmail.com>
2023-01-07 15:48:58 -0300

    13 concluida

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends Component {
+  state = {
+    user: '',
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.userRecover();
+  }
+
+  userRecover = async () => {
+    const user = await getUser();
+    this.setState(() => ({
+      loading: true,
+      user,
+    }));
+    this.setState(() => ({
+      loading: false,
+    }));
+  };
+
   render() {
+    const { loading, user } = this.state;
+    const { email, description, image, name } = user;
     return (
       <div data-testid="page-profile">
+        {loading && Loading}
         <Header />
+        <div>
+          <img src={ image } alt="user" data-testid="profile-image" />
+        </div>
+        <div>
+          <h2>Nome</h2>
+          <p>{name}</p>
+        </div>
+        <div>
+          <h2>Email</h2>
+          <p>{email}</p>
+        </div>
+        <div>
+          <h2>Descrição</h2>
+          <p>{description}</p>
+        </div>
+        <Link to="/profile/edit"> Editar perfil </Link>
       </div>
     );
   }
